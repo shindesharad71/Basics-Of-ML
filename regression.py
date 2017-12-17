cf52f0ef24c2f640982b19948df09059f8691d9b
Sharad Shinde <shindesharad71@gmail.com>
2017-12-17 15:43:07 +0530

    Linear Regression Classifier

--- regression.py
+++ regression.py
@@ -2,6 +2,9 @@
 
 import pandas as pd
 import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = "1xfQzjvZyaQZkyHgdUm3" # Use Your Own API Key!
 df = quandl.get('WIKI/GOOGL')
@@ -21,4 +24,15 @@ forcast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forcast_col].shift(-forcast_out)
 df.dropna(inplace=True)
 
-print(df.head())
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1) # As Many Jobs as possible by CPU
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
