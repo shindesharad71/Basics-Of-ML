412db1d9e42077b2651e50d19d2d498add1c6644
Sharad Shinde <shindesharad71@gmail.com>
2017-12-17 19:41:57 +0530

    scratch regression complete

--- LenearRegression_From_Scratch.py
+++ LenearRegression_From_Scratch.py
@@ -1,14 +1,23 @@
 from statistics import mean
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+style.use('fivethirtyeight')
 
-def best_fit_slope(xs, ys):
+xs = np.array([1,2,3,4,5,6], dtype= np.float64)
+ys = np.array([5,4,6,5,6,7], dtype= np.float64)
+
+def best_fit_slope_and_intercept(xs, ys):
     # Linear Regression Formula
-    m = ( ((mean(xs) * mean(ys)) - mean(xs*ys)) / 
-        ((mean(xs)*mean(xs)) - mean(xs*xs)) )
-    return m
+    m = ( ((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs)*mean(xs)) - mean(xs*xs)) )
+    b = mean(ys) - m*mean(xs)
+    return m, b
 
-m = best_fit_slope(xs, ys)
-print(m)
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+regression_line = [ (m*x)+b for x in xs ]
+
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
+plt.show()
